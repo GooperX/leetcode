f61eba8c0d2f6b9681c4cbbe4f83770430c2dba4
gooper <1318612722@qq.com>
2022-03-19 00:00:58 +0800

    feat: 编号53

---
+++ src/simple/maxSubArray/index.ts
@@ -0,0 +1,32 @@
+
+/**
+ * 53. 
+ * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * 子数组 是数组中的一个连续部分。
+ * 
+ * @example
+ * 输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6 。
+ * 
+ * 输入: nums = [1]
+ * 输出: 1
+ * 
+ * 输入: nums = [5,4,-1,7,8]
+ * 输出: 23
+ * 
+ * @param nums 整数数组
+ * @returns 
+ */
+export const maxSubArray = (nums: number[]): number => {
+
+  let pre = 0
+  let max = nums[0]
+
+  nums.forEach(num => {
+    pre = Math.max(pre + num, num)
+    max = Math.max(pre, max)
+  })
+
+  return max
+}
---
+++ tests/simple/maxSubArray/index.spec.ts
@@ -0,0 +1,18 @@
+import { maxSubArray } from "../../../src/simple/maxSubArray"
+
+
+
+describe('maxSubArray', () => {
+
+  test('[-2,1,-3,4,-1,2,1,-5,4]', () => {
+    expect(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])).toBe(6)
+  })
+
+  test('[1]', () => {
+    expect(maxSubArray([1])).toBe(1)
+  })
+
+  test('[5,4,-1,7,8]', () => {
+    expect(maxSubArray([5,4,-1,7,8])).toBe(23)
+  })
+})
